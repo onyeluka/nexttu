2312d430b123ec8532e1048571464f7924f5cc5b
pharmluka <onyelukaokonkwo@gmail.com>
2023-04-05 13:01:08 +0300

    feat: changed footer element

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,5 +1,5 @@
 import { tw } from 'twind';
-import Button from '@/components/button';
+// import Button from '@/components/button';
 
 const productLinks = [`Features`, `Customers`, `Platform`, `Pricing`, `Enterprise`, `What's new?`];
 const aboutLinks = [`About Us`, `Careers`, `Leadership`, `Blog`, `Events`, `Press`];
@@ -14,17 +14,18 @@ const resourceLinks = [
   `Training`,
   `Other resources`,
 ];
+const socialLinks = [`Facebook`, `Youtube`, `LinkedIn`, `Webinar`];
+const sellerFor = [`Business`, `individual`];
 
 const Footer = () => (
   <footer className={tw(`bg-white border-t border-gray-400 pt-14 pb-16`)}>
     <div className={tw(`max-w-7xl mx-auto text-gray-400 px-8 lg:px-0 flex flex-wrap`)}>
       <div className={tw(`mb-14 flex items-center w-full`)}>
         <img className={tw(`h-22 w-22 mr-4`)} src="logo.png" alt="logo" width={128} height={128} />
-        {/* <p className={tw(`text-4xl text-indigo-500 font-bold`)}>STARTD</p> */}
       </div>
-      <div className={tw(`w-full lg:w-1/2`)}>
+      <div className={tw(`w-full lg:w-5/5`)}>
         <ul className={tw(`text-lg font-light flex flex-wrap w-full`)}>
-          <li className={tw(`w-1/2 md:w-1/3 lg:w-1/3`)}>
+          <li className={tw(`w-1/2 md:w-1/3 lg:w-1/5`)}>
             <div>
               <h4 className={tw(`text-gray-900 text-base font-bold mb-1`)}>Product</h4>
               <ul>
@@ -36,7 +37,7 @@ const Footer = () => (
               </ul>
             </div>
           </li>
-          <li className={tw(`w-1/2 md:w-1/3 lg:w-1/3`)}>
+          <li className={tw(`w-1/2 md:w-1/3 lg:w-1/5`)}>
             <div>
               <h4 className={tw(`text-gray-900 text-base font-bold mb-1`)}>Resources</h4>
               <ul>
@@ -48,7 +49,7 @@ const Footer = () => (
               </ul>
             </div>
           </li>
-          <li className={tw(`w-1/2 md:w-1/3 lg:w-1/3`)}>
+          <li className={tw(`w-1/2 md:w-1/3 lg:w-1/5`)}>
             <div>
               <h4 className={tw(`text-gray-900 text-base font-bold mb-1`)}>About Us</h4>
               <ul>
@@ -60,9 +61,33 @@ const Footer = () => (
               </ul>
             </div>
           </li>
+          <li className={tw(`w-1/2 md:w-1/3 lg:w-1/5`)}>
+            <div>
+              <h4 className={tw(`text-gray-900 text-base font-bold mb-1`)}>SellerSea For</h4>
+              <ul>
+                {sellerFor.map((link) => (
+                  <li className={tw(`text-gray-800 text-sm font-medium leading-6`)} key={link}>
+                    <a href="/">{link}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </li>
+          <li className={tw(`w-1/2 md:w-1/3 lg:w-1/5`)}>
+            <div>
+              <h4 className={tw(`text-gray-900 text-base font-bold mb-1`)}>Follow Us</h4>
+              <ul>
+                {socialLinks.map((link) => (
+                  <li className={tw(`text-gray-800 text-sm font-medium leading-6`)} key={link}>
+                    <a href="/">{link}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </li>
         </ul>
       </div>
-      <div className={tw(`w-full lg:w-1/2 mt-12 lg:mt-0`)}>
+      {/* <div className={tw(`w-full lg:w-1/2 mt-12 lg:mt-0`)}>
         <div className={tw(`border border-gray-400 rounded py-5 px-4`)}>
           <h4 className={tw(`font-mono text-sm uppercase text-gray-500 mb-3`)}>Subscribe our newsletter</h4>
           <div className={tw(`flex w-full`)}>
@@ -75,7 +100,7 @@ const Footer = () => (
             <Button>Subscribe</Button>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   </footer>
 );
